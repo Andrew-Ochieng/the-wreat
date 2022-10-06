2582ffe5e842c173db833974e20646097dd559b9
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-07 00:44:04 +0300

    add footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,19 @@ const Footer = () => {
     return ( 
         <div>
             <footer className="bg-gray-900 ">
-                <div className="md:py-8 md:px-16 p-6 md:flex justify-between"> 
-                    <div className="text-gray-100 space-y-2">
-                        <h1>The Wreat</h1>
-                        <p>Lorem ipsum dolor sit.</p>
-                        <div className="md:space-x-4">
-                            <i className="fa fa-facebook"></i>
-                            <i className="fa fa-instagram"></i>
-                            <i className="fa fa-linkedin"></i>
-                            <i className="fa fa-github"></i>
+                <div className="md:p-16 p-6 md:flex justify-around "> 
+                    <div className="text-gray-100 space-y-2 md:my-0 my-6">
+                        <h1 className="md:text-xl text-lg font-semibold uppercase text-green-500">The Wreat</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur.</p>
+                        <div className="space-x-4 text-xl">
+                            <i className="hover:text-green-500 fa fa-facebook"></i>
+                            <i className="hover:text-green-500 fa fa-instagram"></i>
+                            <i className="hover:text-green-500 fa fa-linkedin"></i>
+                            <i className="hover:text-green-500 fa fa-github"></i>
                         </div>
                     </div>
-                    <div className="text-gray-100 space-y-2">
-                        <h3 className="font-medium md:text-xl text-lg">Topics</h3>
+                    <div className="text-gray-100 space-y-2 md:my-0 my-6">
+                        <h3 className="font-medium text-green-500 md:text-xl text-lg">About</h3>
                         <ul className="space-y-1">
                             <li>Energy</li>
                             <li>Agriculture</li>
@@ -23,24 +23,23 @@ const Footer = () => {
                             <li>Waste Management</li>
                         </ul>
                     </div>
-                    <div className="text-gray-100 space-y-2">
-                        <h3 className="font-medium md:text-xl text-lg">About</h3>
+                    <div className="text-gray-100 space-y-2 md:my-0 my-6">
+                        <h3 className="font-medium text-green-500 md:text-xl text-lg">Continents</h3>
                         <ul className="space-y-1">
-                            <li>Links</li>
-                            <li>Links</li>
-                            <li>Links</li>
-                            <li>Links</li>
-                            <li>Links</li>
+                            <li>Africa</li>
+                            <li>Asia</li>
+                            <li>North America</li>
+                            <li>South America</li>
+                            <li>Europe</li>
+                            <li>Antarctica</li>
                         </ul>
                     </div>
-                    <div className="text-gray-100 space-y-2">
-                        <h3 className="font-medium md:text-xl text-lg">Support</h3>
+                    <div className="text-gray-100 space-y-2 md:my-0 my-6">
+                        <h3 className="font-medium text-green-500 md:text-xl text-lg">Support</h3>
                         <ul className="space-y-1">
                             <li>Contact</li>
                             <li>Help</li>
                             <li>FAQ</li>
-                            <li>Links</li>
-                            <li>Links</li>
                         </ul>
                     </div>
                 </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,13 +14,13 @@ const Home = ({ energyData, transportData }) => {
           }
         </div>
 
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-8 gap-4">
+        {/* <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-8 gap-4">
           {
                transportData.map((transport) => {
                     return <BlogCard transport={transport} />
                })
           }
-        </div>
+        </div> */}
      </div>
      );
 }
